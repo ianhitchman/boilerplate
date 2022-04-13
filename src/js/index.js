142cfc116a5ac4acc33c1d8339b005ef2599e90b
Ian Hitchman <ian@w3a.co.uk>
2022-04-13 12:51:46 +0000

    Initial commit

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+// Import all Bootstrap plugins, or select individually
+import * as bootstrap from "bootstrap";
+
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return <h2>Have a nice day!</h2>;
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
